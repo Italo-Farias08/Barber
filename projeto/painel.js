68f3ae6127e523eabbee4d63d9ac1868ed907a32
Italo-Fariasa <italo.farias1wi@gmail.com>
2026-04-16 05:09:24 -0300

    fcs

--- projeto/painel.js
+++ projeto/painel.js
@@ -1,11 +1,7 @@
 const lista = document.getElementById("lista");
-const horarios = [
-  "08:00", "09:00", "10:00", "11:00",
-  "12:00", "13:00", "14:00", "15:00",
-  "16:00", "17:00", "18:00"
-];
+
 function carregarAgendamentos() {
-  fetch("https://barber-7p3h.onrender.com")
+  fetch("https://barber-7p3h.onrender.com/agendamentos")
     .then(res => res.json())
     .then(dados => {
 
@@ -23,28 +19,23 @@ function carregarAgendamentos() {
         const div = document.createElement("div");
         div.classList.add("card");
 
-        // 🔥 se algum dia tiver status
-        if (item.status === "ocupado") {
-          div.classList.add("ocupado");
-        }
-
         const info = document.createElement("div");
         info.classList.add("info");
 
-       const data = item.data;
-const horario = item.horario.toString().substring(0,5);
+        const data = item.data;
+        const horario = item.horario.toString().substring(0,5);
 
-info.innerHTML = `
-  <div>👤 <strong>${item.nome || "Sem nome"}</strong></div>
-  <div>📅 ${data}</div>
-  <div>⏰ ${horario}</div>
-`;
+        info.innerHTML = `
+          <div>👤 <strong>${item.nome || "Sem nome"}</strong></div>
+          <div>📅 ${data}</div>
+          <div>⏰ ${horario}</div>
+        `;
 
         const btn = document.createElement("button");
         btn.innerText = "❌ Cancelar";
 
         btn.onclick = () => {
-          fetch(`http://localhost:3000/agendamentos/${item.id}`, {
+          fetch(`https://barber-7p3h.onrender.com/agendamentos/${item.id}`, {
             method: "DELETE"
           })
           .then(() => div.remove());
