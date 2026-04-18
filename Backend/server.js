15fd89c194bcadddc430ccf162f94592778c7c69
Italo-Fariasa <italo.farias1wi@gmail.com>
2026-04-17 23:54:12 -0300

    23

--- Backend/server.js
+++ Backend/server.js
@@ -227,6 +227,8 @@ app.delete("/gastos/:id", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Servidor rodando na porta", PORT);
 });
